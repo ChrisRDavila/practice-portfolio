9baf08f19adaee5081c4566c22fa01f80672ce87
ChrisRDavila <crossdavila@gmail.com>
2024-01-01 23:32:04 -0800

    Add animation on letters for page open and hover, debugged 'destroy' error

---
+++ src/components/Home/index.js
@@ -0,0 +1,50 @@
+import { Link} from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-r.png';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['o','s','s', ' ', 'D', 'a', 'v', 'i', 'l', 'a']
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r','.']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  //recieving an error destroy is not a functiobat safelyCallDestroy
+  //fixed by removing return for setTimeout
+
+  return (
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i, </span>
+            <br />
+            <span className={`${letterClass} _13`}> I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+
+          <img 
+          src={LogoTitle} 
+          alt='developer' 
+          />
+          <AnimatedLetters letterClass={letterClass} 
+          strArray={nameArray} 
+          idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass} 
+          strArray={jobArray} 
+          idx={26} />
+          </h1>
+          <h2>Full-stack developer / Student / Musician</h2>
+          <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+        </div>
+      </div>
+  );
+}
+
+export default Home;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,21 @@
 import { Link, NavLink} from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import LogoR from '../../assets/images/logo-r.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
-const SideBar = () => {
+const SideBar = () => (
   <div className="nav-bar">
     <Link className='logo' to='/'>
-      <img src={LogoS} alt='logo' />
-      <img className='sub-logo' src={LogoSubtitle} alt='slobodon' />
+      <img src={LogoR} alt='logo' />
+      <h1 className="sub-logo">ChrisRDavila</h1>
     </Link>
     <nav>
-      <NavLink exact='true' activeclassname='active' to='/'>
+      <NavLink 
+        exact='true' 
+        activeclassname='active' 
+        to='/'>
         <FontAwesomeIcon icon={faHome} color='4d4d4e' />
       </NavLink>
       <NavLink 
@@ -22,11 +25,31 @@ const SideBar = () => {
       to='/about'>
         <FontAwesomeIcon icon={faUser} color='4d4d4e' />
       </NavLink>
-      <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
+      <NavLink 
+        exact='true' 
+        activeclassname='active' 
+        className='contact-link' 
+        to='/contact'>
         <FontAwesomeIcon icon={faEnvelope} color='4d4d4e' />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target='_blank' 
+        rel='noreferrer' 
+        href='https://www.linkedin.com/in/christopher-davila-004408269/'>
+          <FontAwesomeIcon icon={faLinkedin} color='4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a target='_blank' 
+        rel='noreferrer' 
+        href='https://github.com/ChrisRDavila'>
+          <FontAwesomeIcon icon={faGithub} color='4d4d4e' />
+        </a>
+      </li>
+    </ul>
   </div>
-}
+)
 
 export default SideBar;
